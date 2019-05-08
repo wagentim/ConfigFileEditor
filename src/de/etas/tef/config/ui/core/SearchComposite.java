722391e0e22a17dd82aec249d91db29ab4f5564a
UIH9FE <UIH9FE@FE-Z1K09.fe.de.bosch.com>
2019-05-08 20:13:45 +0200

    include jar package into the project

---
+++ src/de/etas/tef/config/ui/core/SearchComposite.java
@@ -0,0 +1,15 @@
+package de.etas.tef.config.ui.core;
+
+import org.eclipse.swt.widgets.Composite;
+
+public class SearchComposite extends Composite
+{
+
+	
+	
+	public SearchComposite(Composite parent, int style)
+	{
+		super(parent, style);
+	}
+
+}
